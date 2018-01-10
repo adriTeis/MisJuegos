e72256a51812f1ba89d7b4daad936c6f06e0bab9
adriTeis <adrianmv94@hotmail.com>
2018-01-10 20:01:59 +0100

    segundoIntento

--- app/src/main/java/com/example/adrianmontes/miprimerjuego/GameView.java
+++ app/src/main/java/com/example/adrianmontes/miprimerjuego/GameView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
@@ -14,10 +15,33 @@ import android.view.View;
 
 public class GameView extends SurfaceView {
     private Bitmap bmp;
-
+    private SurfaceHolder holder;
 
     public GameView(Context context) {
         super(context);
+        //ahora en vez de gestionar cada vez que hacemos un cambio llamar al onDraw que es lo que hace el View, con el
+        //surfaceView cada vez que hacemos un cambio lo hacemos en surfaceChange
+        holder= getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas c = holder.lockCanvas(null);
+                onDraw(c);
+                holder.unlockCanvasAndPost(c);
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
         bmp= BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
 
     }
